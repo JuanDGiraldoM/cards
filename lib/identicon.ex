144947a748197904ef84e6baca1eeb689c1b9670
JuanDGiraldoM <juandagiraldoma@gmail.com>
2021-01-12 22:17:55 -0500

    Identicon

---
+++ lib/identicon.ex
@@ -0,0 +1,7 @@
+defmodule Identicon do
+  def main(input) do
+  end
+
+  def hash_input do
+  end
+end
---
+++ test/identicon_test.exs
@@ -0,0 +1,8 @@
+defmodule IdenticonTest do
+  use ExUnit.Case
+  doctest Identicon
+
+  test "simple test" do
+    assert 1 == 1
+  end
+end
