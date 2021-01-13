a4cdcb384ac5fd0d4f3fd11149b8405ed335d6bd
JuanDGiraldoM <juandagiraldoma@gmail.com>
2021-01-12 22:35:57 -0500

    Image struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,7 +1,13 @@
 defmodule Identicon do
   def main(input) do
+    input |> hash_input()
   end
 
-  def hash_input do
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
   end
 end
--- test/identicon_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule IdenticonTest do
-  use ExUnit.Case
-  doctest Identicon
-
-  test "simple test" do
-    assert 1 == 1
-  end
-end
